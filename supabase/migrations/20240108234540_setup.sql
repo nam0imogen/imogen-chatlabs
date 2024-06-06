a47152755cec36641abcc687f99bad586955a117
nam0imogen <nam0.imogen@Gmail.com>
2024-06-06 22:26:23 +0800

    Update 20240108234540_setup.sql

--- supabase/migrations/20240108234540_setup.sql
+++ supabase/migrations/20240108234540_setup.sql
@@ -50,7 +50,7 @@ LANGUAGE 'plpgsql'
 SECURITY DEFINER
 AS $$
 DECLARE
-  project_url TEXT := 'https://ukrdhiywigevlawgvmbj.supabase.co';
+  project_url TEXT := 'https://lqpdhvfadtfrcxctftyk.supabase.co';
   service_role_key TEXT := 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrcmRoaXl3aWdldmxhd2d2bWJqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcwNzI1MjAzMiwiZXhwIjoyMDIyODI4MDMyfQ.rCz1Zqjy0cCZXzW8idiLsjAyz5fZxy7mMQKau02BQ_s'; -- full access needed for http request to storage
   url TEXT := project_url || '/storage/v1/object/' || bucket || '/' || object;
 BEGIN
